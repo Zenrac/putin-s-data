25ec5ae6e817f4f31fc85c0144245a196c1f03ee
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-05 15:14:58 +0300

    making server specific store

--- cogs/store.py
+++ cogs/store.py
@@ -8,6 +8,9 @@ class Store():
     def __init__(self, bot):
         self.bot = bot
     
+    @commands.group(hidden=True)
+    async def store(self, ctx):
+        pass
     
 def setup(bot):
     bot.add_cog(Store(bot))
