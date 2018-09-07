c4a901b5e614ec68dc8f097745705b9e62bbc999
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-07 07:10:49 +0300

    fixing problems

--- cogs/store.py
+++ cogs/store.py
@@ -20,8 +20,8 @@ class StoreConfig():
                         _[2]) for _ in (
                             record['item_id'],
                             record['price'],
-                            record['seller_id']
-                            if record['id'] == self.id))
+                            record['seller_id'])
+                            if record['id'] == self.id)
         else:
             self.items = None
         return self.items
