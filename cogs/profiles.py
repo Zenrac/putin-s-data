92c021dbf067875cc1e0e564fd76a7e779c9acab
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-07 18:28:51 +0300

    added profile banner

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -112,8 +112,6 @@ class Profile():
         pet = record['pet'] if record['pet'] else 'No pet'
         pet_title = 'Pet'
         e.add_field(name=pet_title, value=pet, inline=True)
-        #e.set_footer(text='Sorry guys for the database reset, message iWeeti#4990 and he will give you $10K to start with.')
-        e.set_footer(text='Sorry guys for the database reset, message iWeeti#4990 and he will give you $10K to start with.')
         banner = record['banner'] or 0
         banners = {
             0: 'https://cdn.pixabay.com/photo/2016/08/03/09/03/universe-1566159_960_720.jpg',
