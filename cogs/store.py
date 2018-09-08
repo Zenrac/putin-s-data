84a20711b5f94f445f0c775487009f32c6ffaa63
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-08 17:18:46 +0300

    updated store

--- cogs/store.py
+++ cogs/store.py
@@ -7,14 +7,23 @@ class StoreConfig():
         self.id = guild_id
         self.bot = bot
         
-        _items = []
-        for (id, price, item_id, seller_id, selling_id) in record['id'], record['price'], record['item_id'], record['seller_id'], record['selling_id']:
-            _items.append(id, price, item_id, seller_id, selling_id)
-        self._items = _items
+        # _items = []
+        # for (id, price, item_id, seller_id, selling_id) in (record['id'], record['price'], record['item_id'], record['seller_id'], record['selling_id']):
+        #     _items.append(id, price, item_id, seller_id, selling_id)
+        # self._items = _items
+
+        self._items = None
+
+        self._foo = type(record)
 
     @property
     def items(self):
-        return self._items    
+        return self._items
+
+    @property
+    def foo(self):
+        return self._foo
+    
     
 class Store():
     def __init__(self, bot):
