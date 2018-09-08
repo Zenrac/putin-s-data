beee39058b797ba4011b9c79f70b540b98749e9c
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-08 17:38:30 +0300

    updated store

--- cogs/store.py
+++ cogs/store.py
@@ -9,7 +9,8 @@ class StoreConfig():
         
         _items = []
         for _record in record:
-            _items.append(_record['id'], _record['price'], _record['item_id'], _record['seller_id'], _record['selling_id'])
+            # _items.append(_record['id'], _record['price'], _record['item_id'], _record['seller_id'], _record['selling_id'])
+            _items.append(type(_record))
 
         self._foo = record
 
