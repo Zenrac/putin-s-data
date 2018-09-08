926d27944ad2e547b966bb8efc2557b07373b10b
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-08 15:05:55 +0300

    fixed mute hopefully

--- cogs/mod.py
+++ cogs/mod.py
@@ -539,6 +539,7 @@ class Mod():
                 permissions = discord.Permissions()
                 permissions.send_messages = False
                 permissions.speak = False
+                await ctx.send(permissions)
                 role = await ctx.guild.create_role(name='Muted', permissions=permissions, mentionable=False, hoist=False)
             await user.add_roles(role, reason=f"Muted by {ctx.author.display_name}(ID:{ctx.author.id})")
             await ctx.send('Muted {}.'.format(user.display_name))
