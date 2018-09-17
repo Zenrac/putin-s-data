f2124efcf07e6c09e3e5611d0f3c2c29f40af1c3
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-17 16:46:02 +0300

    .

--- cogs/afk.py
+++ cogs/afk.py
@@ -15,9 +15,14 @@ class AFK:
 		if '@' in reason:
 			return await ctx.send('You can\'t have `@` in the reason.')
 		await ctx.db.execute(f'insert into afk values({ctx.author.id}, \'{reason}\', \'{when}\')')
+		await ctx.send(f'Have nice time away fromyour keyboard {ctx.author.display_name} :wave:')
 
 	async def on_message(self, message):
-		record = await self.bot.pool.execute(f'select reason, when from afk where id={message.author.id};')
+		try:
+			record = await self.bot.pool.execute(f'select reason, when from afk where id={message.author.id};')
+		except Exception as e:
+			if message.channel.id = 482188217400033280:
+				await message.channel.send(e)
 		if not record: return
 		if not record[0]: return
 		await self.bot.pool.execute(f'delete from afk where id={message.id};')
