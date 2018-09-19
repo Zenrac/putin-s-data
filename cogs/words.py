e97dd033c38135848b0209a166c0c2b1c48ff350
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-19 20:50:03 +0300

    .

--- cogs/words.py
+++ cogs/words.py
@@ -30,12 +30,12 @@ class Blacklist:
 
 	async def on_message(self, message):
 		# if not check_perms(): return
+		ctx = await self.bot.get_context(message)
 		settings = await self.get_settings(ctx.guild.id)
 		settings.words = eval(settings.words)
 		if not settings.blacklist: return
 
 		try:
-			ctx = await self.bot.get_context(message)
 			words = message.content.split()
 			for word in words:
 				if word.lower() in settings.words:
