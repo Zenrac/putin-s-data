000d9e694c1c8f8ca6aa6aebd1284c9387f44f2d
iWeeti <maaninkaveeti@gmail.com>
2018-09-29 21:06:44 +0300

    updating webhooks

--- cogs/whtest.py
+++ cogs/whtest.py
@@ -17,7 +17,11 @@ class WHTest:
 	@commands.command()
 	@checks.is_mod()
 	async def wh(self, ctx):
-		await self.whsend('https://discordapp.com/api/webhooks/495654255470968842/gJge2raRFT6AKa1jInJCliZPJ1RwxdqR20XRogM_LK-VByZUQYywvcKiWOenzn3a35n0', ctx.author.display_name, ctx.author.avatar_url)
+		await self.whsend(
+			'https://discordapp.com/api/webhooks/495654255470968842/gJge2raRFT6AKa1jInJCliZPJ1RwxdqR20XRogM_LK-VByZUQYywvcKiWOenzn3a35n0',
+			ctx.author.display_name,
+			ctx.message.clean_content,
+			ctx.author.avatar_url)
 
 def setup(bot):
 	bot.add_cog(WHTest(bot))
